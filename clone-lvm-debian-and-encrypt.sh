aa36e7c022fb476be5a4954149df9461fdd23790
dima-ch <r3z.mail@gmail.com>
2017-02-10 01:36:14 +0300

    exit

--- clone-lvm-debian-and-encrypt.sh
+++ clone-lvm-debian-and-encrypt.sh
@@ -15,15 +15,16 @@
 # TODO скрипт нуждается в проверках выполения действий!
 
 function usage {
-	echo "usage </dev/disk> <vg_name> <lv_name> [--encrypt] \
-		[--snapshot-size size] [--no-rsync] [--extend-dev /dev/...] \
+	echo "usage </dev/disk> <vg_name> <lv_name> [--encrypt-passphrase passphrase] [--encrypt] \
+		[--snapshot-size size] [--no-rsync] [--extend-dev /dev/...] [--force] \
 		[--root-size size] [--swap-size size] [--new-hostname hostname] \
-		[--add-crypttab str] [--add-fstab str]"
+		[--add-crypttab str] [--add-fstab str] [--exclude-dirs '/dir1 /dir2']"
 	echo "/dev/disk - диск на который будет клонироваться система, на нем будет создано 2 раздела: boot и остальное место под LVM"
 	echo "vg_name - имя группы томов клонируемой системы"
 	echo "lv_name - имя тома с рутом клонируемой системы"
 	echo "--add-crypttab str - строка добавляемая в /etc/crypttab на склонированной системе"
 	echo "--add-fstab str - строка добавляемая в /etc/fstab на склонированной системе"
+	echo "--exclude-dirs - пропустить каталоги"
 	echo "Внимание! Клонируется только root и boot, скрипт не подойдет если var usr home и т.д. на отдельных разделах"
 	exit 1
 }
@@ -34,7 +35,7 @@ OLD_ROOT_VG=$2
 
 SNAPSHOT_SIZE="200"
 RSYNC_OPTIONS=" --one-file-system  -a " # -v --progress 
-
+ROOTMNTTMP="rootmnttmp"
 
 
 if [ ! -b "/dev/${OLD_ROOT_VG}/${OLD_ROOT_LV}" ] || [ ! -e "/dev/${OLD_ROOT_VG}/${OLD_ROOT_LV}" ]  || [ ! -n "$2" ]  || [ ! -n "$3" ]; then
@@ -53,12 +54,15 @@ shift
 
 while [[ $# > 0 ]]
 do
-	key="$1"
-
-	case $key in
-		--encrypt)
+key="$1"
+case $key in
+--encrypt)
 ENCRYPT="TRUE"
 echo "ENCRYPT"
+;;
+--force)
+FORCE_RUN="TRUE"
+echo "FORCE_RUN"
 ;;
 --no-rsync)
 NORSYNC="TRUE"
@@ -73,6 +77,17 @@ EXTEND_DEVICE="$2"
 echo "EXTEND_DEVICE $EXTEND_DEVICE"
 shift
 ;;
+--encrypt-passphrase)
+if [  $# == 1 ]; then
+        echo "MISSING PASSPHRASE"
+        usage
+fi
+ENCRYPT="TRUE"
+PASSPHRASE="$2"
+echo "PASSPHRASE $PASSPHRASE"
+shift
+;;
+
 --new-hostname)
 if [  $# == 1 ]; then
 	echo "MISSING NEW HOSTNAME"
@@ -127,6 +142,21 @@ FSTAB_ADD="$2"
 echo "FSTAB_ADD $FSTAB_ADD"
 shift
 ;;
+--exclude-dirs)
+if [  $# == 1 ]; then
+        echo "MISSING EXCLUDE_DIRS"
+        usage
+fi
+EXCLUDE_DIRS=""
+IFS=' ' read -ra DIR <<< "$2"
+for i in "${DIR[@]}"; do
+    EXCLUDE_DIRS="$EXCLUDE_DIRS --exclude=$i"
+done
+RSYNC_OPTIONS="$RSYNC_OPTIONS $EXCLUDE_DIRS"
+echo "EXCLUDE_DIRS rsync format $EXCLUDE_DIRS"
+echo "RSYNC_OPTIONS $RSYNC_OPTIONS"
+shift
+;;
 *)
 usage
 ;;
@@ -134,15 +164,48 @@ esac
 shift
 done
 
+function safe_exit {
+	umount $ROOTMNTTMP/dev
+	umount $ROOTMNTTMP/proc
+	umount $ROOTMNTTMP/sys
+	umount $ROOTMNTTMP/boot
+	umount $ROOTMNTTMP
+	umount $OLD_ROOT_MNT_TMP
+        umount $BOOT
+	vgreduce --force --removemissing $OLD_ROOT_VG
+        vgremove --force $NEW_VG_NAME
+	/sbin/lvremove --force /dev/$OLD_ROOT_VG/root-snapshot	
+	if [ -n "$EXTEND_DEVICE" ]; then
+        	/sbin/vgreduce --force  $OLD_ROOT_VG $EXTEND_DEVICE
+	fi
+        if [ -n "$ENCRYPT" ]; then
+                /sbin/cryptsetup luksClose $CRYPTNAME
+        fi
+        exit 1
+}
+
 NEW_VG_NAME="root`date +%s`"
 
-echo -e "========Warning!\nPlease disable automount (System-Settings - Details - Removable Media)"
-read -p "Press any key to start"
+if [ ! -n "$FORCE_RUN" ]; then
+	echo -e "========Warning!\nPlease disable automount (System-Settings - Details - Removable Media)"
+	read -p "Press any key to start"
+fi
+
 echo "========create disk part table"
-parted $DISK mktable msdos
+parted --script $DISK mktable msdos
+if [ $? != 0 ];then
+   safe_exit
+fi
 #TODO: размер бута сделать настраевымым
-parted -a optimal $DISK mkpart primary 2048sec 616447sec
-parted -a optimal $DISK mkpart primary 616448sec 100%
+parted --script -a optimal $DISK mkpart primary 2048sec 616447sec
+if [ $? != 0 ];then
+   safe_exit
+fi
+parted --script -a optimal $DISK mkpart primary 616448sec 100%
+if [ $? != 0 ];then
+   safe_exit
+fi
+
 sleep 1
 if [ -e ${DISK}-part1 ]; then
 	BOOT=${DISK}-part1
@@ -182,14 +245,31 @@ fi
 if [ -n "$ENCRYPT" ]; then
 	echo "========ecrypt"
 	if [ ! -f /sbin/cryptsetup ]; then
-    	echo "cryptsetup not found!"
-    	exit 1
+	    	echo "cryptsetup not found!"
+	    	exit 1
 	fi
-	
-	/sbin/cryptsetup luksFormat $NEW_LVM_PARTIRION
-	sleep 2
 	CRYPTNAME="crypt${NEW_VG_NAME}"
-	/sbin/cryptsetup luksOpen $NEW_LVM_PARTIRION $CRYPTNAME
+	if [  -n "$PASSPHRASE" ];then
+		echo -n $PASSPHRASE | /sbin/cryptsetup luksFormat $NEW_LVM_PARTIRION
+		if [ $? != 0 ];then
+		   safe_exit
+		fi
+                sleep 2
+                echo -n $PASSPHRASE | /sbin/cryptsetup luksOpen $NEW_LVM_PARTIRION $CRYPTNAME
+		if [ $? != 0 ];then
+		   safe_exit
+		fi
+	else
+		/sbin/cryptsetup luksFormat $NEW_LVM_PARTIRION
+		if [ $? != 0 ];then
+		   safe_exit
+		fi
+		sleep 2
+		/sbin/cryptsetup luksOpen $NEW_LVM_PARTIRION $CRYPTNAME
+		if [ $? != 0 ];then
+		   safe_exit
+		fi
+	fi
 	REAL_NEW_LVM_PARTIRION=$NEW_LVM_PARTIRION
 	NEW_LVM_PARTIRION=/dev/mapper/$CRYPTNAME
 
@@ -197,20 +277,42 @@ fi
 
 echo "========create lvm"
 pvcreate $NEW_LVM_PARTIRION
+if [ $? != 0 ];then
+	safe_exit
+fi
+
 vgcreate $NEW_VG_NAME $NEW_LVM_PARTIRION
+if [ $? != 0 ];then
+        safe_exit
+fi
 
 if [ -n "$SWAP_SIZE" ]; then
 	lvcreate $NEW_VG_NAME -n swap -L $SWAP_SIZE
+	if [ $? != 0 ];then
+	        safe_exit
+	fi
 	mkswap /dev/mapper/${NEW_VG_NAME}-swap
+	if [ $? != 0 ];then
+	        safe_exit
+	fi
 fi
 
 if [ -n "$ROOT_SIZE" ]; then
 	lvcreate $NEW_VG_NAME -n root -L $ROOT_SIZE
+	if [ $? != 0 ];then
+	        safe_exit
+	fi
 else
 	lvcreate $NEW_VG_NAME -n root -l 100%FREE
+	if [ $? != 0 ];then
+	        safe_exit
+	fi
 fi
 ROOTLVNAME="/dev/${NEW_VG_NAME}/root"
 mkfs.ext4 -m 0 $ROOTLVNAME
+if [ $? != 0 ];then
+        safe_exit
+fi
 
 #-----------------------------------
 ROOTMNTTMP="/mnt/rootmnttmp"
@@ -218,7 +320,10 @@ OLD_ROOT_MNT_TMP="/mnt/old-root-mnt-tmp"
 
 echo "========create old root lvm snapshot"
 if [ -n "$EXTEND_DEVICE" ]; then
-	/sbin/vgextend $OLD_ROOT_VG $EXTEND_DEVICE 
+	/sbin/vgextend $OLD_ROOT_VG $EXTEND_DEVICE
+	if [ $? != 0 ];then
+	        safe_exit
+	fi
 	vgreduce --removemissing $OLD_ROOT_VG
 	SNAPSHOT_SIZE=$(( (`/sbin/blockdev --getsize64 $EXTEND_DEVICE`/1024)/1024 - 4 ))
 	SNAPSHOT_SIZE="${SNAPSHOT_SIZE}M"
@@ -226,78 +331,96 @@ fi
 
 /sbin/lvcreate --snapshot -n root-snapshot -L ${SNAPSHOT_SIZE} /dev/${OLD_ROOT_VG}/${OLD_ROOT_LV}
 if [ $? != 0 ];then
-	echo "LVCREATE SNAPSHOT FAILED!";
-	vgremove $NEW_VG_NAME
-	if [ -n "$ENCRYPT" ]; then
-		/sbin/cryptsetup luksClose $CRYPTNAME
-	fi
-	exit 1;
+	safe_exit
 fi
 
 echo "========mount old root snapshot"
 mkdir $OLD_ROOT_MNT_TMP
 mount -o ro /dev/$OLD_ROOT_VG/root-snapshot $OLD_ROOT_MNT_TMP
+if [ $? != 0 ];then
+	safe_exit
+fi
+
 sleep 1
 
 
-read -p "Press any key to mount new root" 
 mkdir $ROOTMNTTMP
 echo "========mount new root"
 mount $ROOTLVNAME $ROOTMNTTMP
+if [ $? != 0 ];then
+	safe_exit
+fi
 
-read -p "Press any key to rsync root" 
+if [ ! -n "$FORCE_RUN" ]; then
+	read -p "Press any key to rsync root"
+fi
 echo "========rsync root"
 if [ ! -n "$NORSYNC" ]; then
 	rsync $RSYNC_OPTIONS $OLD_ROOT_MNT_TMP/* $ROOTMNTTMP/
 	if [ $? != 0 ];then
-		echo "RSYNC ERROR! press S to skip";
+		if [ -n "$FORCE_RUN" ]; then
+			safe_exit
+		fi
+
+		echo "RSYNC ERROR! press s to skip";
 	        read -s -n 1 KEY
-	        if [[ $KEY = "S" ]]; then
+	        if [[ $KEY = "s" ]]; then
 			echo "SKIP RSYNC ERROR"
 	        else
-       			RSYNC_ERROR=1
+       			safe_exit
 	        fi
 	fi
 fi
 
 echo "========umount old root snapshot"
 umount $OLD_ROOT_MNT_TMP
+if [ $? != 0 ];then
+        safe_exit
+fi
 
 echo "========remove old root lvm snapshot"
-echo y | /sbin/lvremove /dev/$OLD_ROOT_VG/root-snapshot
+/sbin/lvremove --force /dev/$OLD_ROOT_VG/root-snapshot
+if [ $? != 0 ];then
+        safe_exit
+fi
+
 if [ -n "$EXTEND_DEVICE" ]; then
-	/sbin/vgreduce  $OLD_ROOT_VG $EXTEND_DEVICE
+	/sbin/vgreduce $OLD_ROOT_VG $EXTEND_DEVICE
+	if [ $? != 0 ];then
+        	safe_exit
+	fi
 fi
 
-if [ ! -n "$RSYNC_ERROR" ]; then
-	echo "========mkfs new boot"
-	mkfs.ext2 -m 5 $BOOT
-	echo "========mount new boot"
-	mount $BOOT $ROOTMNTTMP/boot
+echo "========mkfs new boot"
+mkfs.ext2 -m 5 $BOOT
+if [ $? != 0 ];then
+	safe_exit
+fi
+echo "========mount new boot"
+mount $BOOT $ROOTMNTTMP/boot
+if [ $? != 0 ];then
+	safe_exit
+fi
 
+if [ ! -n "$FORCE_RUN" ]; then
 	read -p "Press any key to rsync boot" 
-	echo "========rsync boot"
-	if [ ! -n "$NORSYNC" ]; then
-		rsync $RSYNC_OPTIONS  /boot/* $ROOTMNTTMP/boot
-		if [ $? != 0 ];then
-	                echo "RSYNC ERROR! press S to skip"
-			read -s -n 1 KEY
-			if [[ $KEY = "S" ]]; then
-                        	echo "SKIP RSYNC ERROR"
-        	        else
-                	        RSYNC_ERROR=1
-	                fi
-		fi
-	fi
 fi
+echo "========rsync boot"
+if [ ! -n "$NORSYNC" ]; then
+	rsync $RSYNC_OPTIONS  /boot/* $ROOTMNTTMP/boot
+	if [ $? != 0 ];then
+                if [ -n "$FORCE_RUN" ]; then
+                        safe_exit
+                fi
 
-if [ -n "$RSYNC_ERROR" ]; then
-	umount $ROOTMNTTMP
-	vgremove $NEW_VG_NAME
-	if [ -n "$ENCRYPT" ]; then
-		/sbin/cryptsetup luksClose $CRYPTNAME
+                echo "RSYNC ERROR! press s to skip"
+		read -s -n 1 KEY
+		if [[ $KEY = "s" ]]; then
+                       	echo "SKIP RSYNC ERROR"
+       	        else
+               	        safe_exit
+                fi
 	fi
-	exit 1;
 fi
 
 echo "========update crypttab and fstab"
@@ -322,15 +445,37 @@ fi
 
 echo "========mount dev proc sys"
 mount -o bind /dev $ROOTMNTTMP/dev
+if [ $? != 0 ];then
+	safe_exit
+fi
+
 mount -t proc none $ROOTMNTTMP/proc
+if [ $? != 0 ];then
+        safe_exit
+fi
+
 mount -t sysfs none $ROOTMNTTMP/sys
+if [ $? != 0 ];then
+        safe_exit
+fi
 
 echo "========install grub"
 chroot $ROOTMNTTMP grub-install ${DISK}
+if [ $? != 0 ];then
+        safe_exit
+fi
+
 echo "========update-initramfs"
 chroot $ROOTMNTTMP update-initramfs -k all -c
+if [ $? != 0 ];then
+        safe_exit
+fi
+
 echo "========udate grub"
 chroot $ROOTMNTTMP update-grub ${DISK}
+if [ $? != 0 ];then
+        safe_exit
+fi
 
 if [ -n "$NEW_HOSTNAME" ]; then
 	OLD_HOSTNAME=$(cat /etc/hostname)
